e0630a2b48ef4ae604abf0594274c5d774e1cc39
Broxides <skvarkajr@gmail.com>
2023-09-04 14:00:36 +0200

    Completed vid 2

---
+++ Bugporter.API/Functions/ReportBugFunction.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Bugporter.API;
+
+
+namespace Bugporter.API.Functions
+{
+    public class ReportBugFunction
+    {
+        private readonly ILogger<ReportBugFunction> _logger;
+
+        public ReportBugFunction(ILogger<ReportBugFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [FunctionName("ReportBugFunction")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "bugs")] HttpRequest req)
+        {
+
+            return new OkResult();
+        }
+    }
+}
---
+++ Bugporter.API/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Bugporter.API;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Bugporter.API
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+           
+        }
+    }
+}
